76281c4e7d1d125ec4653d68f56c3bed52893ce3
ZacharyTrux <ztruxillo@gmail.com>
2024-05-14 19:23:56 -0500

    yipee!

--- Program 5/GameCharacter.java
+++ Program 5/GameCharacter.java
@@ -1,27 +1,38 @@
+/* name: Zachary Truxillo 
+ * Date: 5/14/2024
+ * Description: A simple java code to help learn syntax, 
+ * it utilizes all major methods taught up to this point and is a simple character class
+*/
 
 
 public class GameCharacter{
+    // Initialize all private variables (only variables the class can use)
     private String name;
     private int lives;
+    // creates the inventory to be an array which is initially five unless given something different 
     private String[] inventory = new String[5];
     private final int MAXLIVES = 5;
     
+    // test class which assigns a default name if the constructor is called 
     public GameCharacter(){
         this.name = "Sam Sung";
         this.lives = MAXLIVES;
     }
 
+    // sets the private variables of the character to a given value
     public GameCharacter(String name, int lives){
         this.name = name;
         setLives(lives);
     }
 
+    // Getters and setters for private variables
     public String getName(){
         return name;
     }
     public void setName(String newName){
         this.name = newName;
     }
+
     public int getLives(){
         return lives;
     }
@@ -33,6 +44,7 @@ public class GameCharacter{
             this.lives = MAXLIVES;
         }
     }
+
     public String[] getInventory(){
         return inventory;
     }
@@ -42,21 +54,26 @@ public class GameCharacter{
         }
     }
 
+    // Returns a true or false value
     public boolean isAlive(){
         return this.lives > 0;
     }
+
+    // flag to check if character has any weapon items
     public boolean hasWeapon(){
+        // iterates through each part of inventory checking for a specific items
         for (String weapon: this.inventory){
             if(weapon == "knife" || weapon == "gun"){
                 return true;
-            }   
+            }
         }
         return false;
     }
 
+    // iterate through the inventory returning spaces taken up except for null spots 
     public int sizeOfInventory(){
         int num = 0;
-        for (int i=0; i<5; i++){
+        for (int i = 0; i < this.inventory.length; i++){
             if(inventory[i] != null){
                 num++;
             }
@@ -64,18 +81,21 @@ public class GameCharacter{
         return num;
     }
 
+    // reset the player's lives
     public void heal(){
         this.lives = MAXLIVES;
     }
 
+    // check player's lives and get subtract a life if greater than 0
     public void damage(){
         if(isAlive()){
             this.lives -= 1;
         }
     }
 
+    // add items into the inventory to the first null(empty) spot found
     public void pickUp(String item){
-        for(int i=0;i<5;i++){
+        for(int i = 0; i < this.inventory.length; i++){
             if(this.inventory[i] == null){
                 this.inventory[i] = item;
                 break;
@@ -83,8 +103,9 @@ public class GameCharacter{
         }
     }
 
+    // gets rid of the given item from the inventory and makes the spot open again
     public void drop(String item){
-        for(int i=0; i<5; i++){
+        for(int i = 0; i < this.inventory.length; i++){
             if(this.inventory[i] == item){
                 this.inventory[i] = null;
                 break;
@@ -92,9 +113,10 @@ public class GameCharacter{
         }
     }
 
+    // returns a string which correctly formats all player information
     public String toString(){
         String str = "Name:\t" + this.name + "\nLives:\t" + this.lives + "\nInventory:\t";
-        for(int i=0; i<5; i++){
+        for(int i = 0; i < this.inventory.length; i++){
             if(this.inventory[i] != null){
                 str += inventory[i]+", ";
             }
